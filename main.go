9b967eeb953af55544e55485aa1afd57c38ed6be
Himanshu Tiwari <ht5620085@gmail.com>
2026-05-07 18:18:13 +0530

    adding url checking with url.Parse from net/url and logging the error with log.Fatal()

--- main.go
+++ main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"fmt"
 	"net/url"
+	"log"
 )
 
 func main() {
 	fmt.Println("Welcome to Video Downloader.")
 
-	var url string
+	var urlInp string
 	fmt.Println("Enter the URL")
-	fmt.Scan(&url)
+	fmt.Scan(&urlInp)
 
-	fmt.Println("The URL you entered is :-", url)
+	fmt.Println("The URL you entered is :-", urlInp)
+
+	// checking if the url is correct
+	u, err := url.Parse(urlInp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(u)
 }
